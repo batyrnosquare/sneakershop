24e6f62110bfd9cab6b91128bf9fd93b04b9a3d3
batyrnosquare <shayrbekov05@gmail.com>
2025-04-07 10:08:51 +0500

    Fix: PaymentService.orderPayment()

--- src/main/java/com/example/sneakershop/repository/OrderRepository.java
+++ src/main/java/com/example/sneakershop/repository/OrderRepository.java
@@ -4,7 +4,10 @@ import com.example.sneakershop.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+    Optional<Orders> findTopByUserIdOrderByIdDesc(Long userId);
 
 }
--- src/main/java/com/example/sneakershop/service/PaymentService.java
+++ src/main/java/com/example/sneakershop/service/PaymentService.java
@@ -6,6 +6,7 @@ import com.example.sneakershop.model.User;
 import com.example.sneakershop.repository.OrderRepository;
 import com.example.sneakershop.repository.PaymentRepository;
 import com.example.sneakershop.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class PaymentService {
 
@@ -35,13 +37,19 @@ public class PaymentService {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid JWT token");
         }
         String username = jwtUtils.extractUsername(jwt);
+        log.info("Username: {}", username);
         Optional<User> user = Optional.ofNullable(userRepository.findByUsername(username)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found")));
-        Orders order = orderRepository.findById(payment.getOrder().getId())
+        log.info("User: {}", user.get().getId());
+        Orders order = orderRepository.findTopByUserIdOrderByIdDesc(user.get().getId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found"));
+        log.info("Order: {}", order.getId());
         if (!order.getUserId().equals(user.get().getId())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You are not authorized to make this payment");
         }
+        payment.setOrder(order);
+
+        orderRepository.save(payment.getOrder());
         payment.setCreatedAt(LocalDateTime.now());
         payment.setAmount(order.getTotalPrice());
         payment.setStatus("PENDING");
@@ -49,7 +57,8 @@ public class PaymentService {
         payment.getOrder().setStatus("PAYMENT PENDING");
         payment.getOrder().setPayment(payment);
 
-        orderRepository.save(payment.getOrder());
         return paymentRepository.save(payment);
     }
+
+
 }
